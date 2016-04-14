e54de581d54837af1e0dfa044248a6584eeb237d
Rahul Sabharwal <rsabharw@ciena.com>
2016-04-14 22:02:21 +0530

    added model as array
    display from template
    used for loop to iterate array

--- app/routes/orders.js
+++ app/routes/orders.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model(){
-        return {id: '1', name: 'Nate'};
+        return [
+             {id: '1', name: 'Nate'},
+             {id: '2', name: 'Gregg'}
+        ];
     }
 });
