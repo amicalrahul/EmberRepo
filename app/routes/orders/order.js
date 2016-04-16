cf6f6d5e242b8ebb09cdbe45d5cc9e10bb99fb9e
Rahul Sabharwal <rsabharw@ciena.com>
2016-04-16 18:00:47 +0530

    Moved the data into the service
    Created 2 methods in service
    Find the service using Ember.inject.service
    use this service in routes to get the data from the 2 mew methods

--- app/routes/orders/order.js
+++ app/routes/orders/order.js
@@ -2,9 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model(params){
-        return [
-             {id: '1', name: 'Nate'},
-             {id: '2', name: 'Gregg'}
-        ].findBy('id', params.order_id);
-    }
+        const store = this.get('store');
+        return store.getOrderById(params.order_id);
+    },
+    store: Ember.inject.service('store')
 });
