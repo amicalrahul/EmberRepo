adaa4356ff6ff76c2e0e0529d0bbc6e260094e2c
Rahul Sabharwal <rsabharw@ciena.com>
2016-04-16 17:50:41 +0530

    Created a new service to store data at center location
    ember generate service store

--- app/router.js
+++ app/router.js
@@ -5,7 +5,7 @@ const Router = Ember.Router.extend({
   location: config.locationType
 });
 
-Router.map(function() {
+Router.map(function() {  
   this.route('orders', function() {
   this.route('order', {path: '/:order_id' } );      
   });
